ac2ac8e40b4d969da0b642199cf8a200f1e794ac
latishasumardy <latishasumardy@gmail.com>
2021-04-14 13:45:25 -0700

    add js

---
+++ Week_02/js/lab1.js
@@ -0,0 +1,16 @@
+// JavaScript const variable declaration
+const map = L.map('map').setView([34.0709, -118.444], 15);
+
+// Leaflet tile layer, i.e. the base map
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+//JavaScript let variable declaration to create a marker
+let marker1 = L.marker([34.0709, -118.444]).addTo(map)
+		.bindPopup('Math Sciences 4328 aka the Technology Sandbox<br> is the lab where I work in ')
+		.openPopup();
+
+let marker2 = L.marker([37.720901, -121.924042]).addTo(map)
+		.bindPopup('Dublin High School<br> is my old high school ')
+		.openPopup();
