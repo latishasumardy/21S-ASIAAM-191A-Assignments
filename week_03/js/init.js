e25df624120b1cc07dc6fd40fffad1574cda9f1c
latishasumardy <latishasumardy@gmail.com>
2021-04-21 13:52:25 -0700

    add new points, change map style

--- week_03/js/init.js
+++ week_03/js/init.js
@@ -1,10 +1,10 @@
-let long = 34.0709;
-let lat = -118.444;
-let zoom = 5;
-const map = L.map('map').setView([long, lat], zoom);
+let coord = [37.712545, -122.035845];
+let zoom = 9.5;
+const map = L.map('map').setView(coord, zoom);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('https://tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 // adding markers
@@ -17,16 +17,16 @@ let home = L.marker([37.7409, -122.484]).addTo(map)
 let random = L.marker([39.7409, -122.484]).addTo(map)
 		.bindPopup('Third Point')
 */
-function createButtons(lat,lng,title){
+function createButtons(lat,lng,title,zoom){
     const newButton = document.createElement("button"); // adds a new button
     newButton.id = "button"+title; // gives the button a unique id
     newButton.innerHTML = title; // gives the button a title
     newButton.setAttribute("lat",lat); // sets the latitude 
     newButton.setAttribute("lng",lng); // sets the longitude 
     newButton.addEventListener('click', function(){
-        map.flyTo([lat,lng]); //this is the flyTo from Leaflet
+        map.flyTo([lat,lng], zoom); //this is the flyTo from Leaflet
     })
-    document.body.appendChild(newButton); //this adds the button to our page.
+    document.getElementById("buttons").appendChild(newButton); //this adds the button to our page.
 }
 
 function addMarker(lat, long, message) {
@@ -36,25 +36,21 @@ function addMarker(lat, long, message) {
     return message;
 }
 
-addMarker(40,-122,"hello world");
-addMarker(34.0709, -118.444,"Where I work on campus");
-addMarker(37.7409, -122.484,"Where I currently am");
-addMarker(39.7409, -122.484,"third point");
-
 function addMarker2(lat, long, message, color) {
-    let favColor = color;
-    console.log("My favorite color is " + favColor);
     L.circleMarker([lat, long], {
         "radius": 10,
         "fillColor": color,
         "color": color,
-        "weight": 1,
-        "opacity": 1
+        "weight": 3,
+        "opacity": 1,
+        "fillOpacity": 0.5
       }).addTo(map).bindPopup(message);
-      createButtons(lat,long,message);
-    return favColor;
+      createButtons(lat,long,message,17);
 }
 
-addMarker2(20,-122,"one", '#000000');
-addMarker2(30.0709, -118.444,"two", '#f542ce');
-addMarker2(35.7409, -122.484,"three", '#ba3e34');
+createButtons(37.712545, -122.035845,"Reset Zoom", 10);
+addMarker2(37.790890, -122.404228,"Muracci's Japanese Curry and Grill", '#000000');
+addMarker2(37.698170, -122.480980,"Shaking Crab", '#f542ce');
+addMarker2(37.722500, -121.942100,"Boba Bliss", '#ba3e34');
+addMarker2(37.762220, -121.950890,"Teaspoon", '#ba3e34');
+addMarker2(37.704570, -121.875450,"Odagada", '#ba3e34');
