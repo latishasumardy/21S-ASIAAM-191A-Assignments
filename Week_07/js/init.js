b78d64fa83ae5d08aafc8d0d62b99d72fda2a9af
latishasumardy <latishasumardy@gmail.com>
2021-05-20 04:09:56 -0700

    change map

--- Week_07/js/init.js
+++ Week_07/js/init.js
@@ -21,12 +21,12 @@ let circleOther = {
 let isUCLAstudent = L.featureGroup();
 let otherStudent = L.featureGroup();
 
-var Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+var OpenStreetMap_HOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
 });
 
-Stadia_OSMBright.addTo(map)
+OpenStreetMap_HOT.addTo(map)
 
 
 function addMarker(data){
